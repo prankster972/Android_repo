e8b7bedf531c123acc143c138fb6ab4ed2b0091e
prankster972 <morozovroman.ist@gmail.com>
2023-12-23 11:37:39 +0400

    greate Shit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,8 +9,6 @@ import android.widget.ListView;
 import android.widget.ArrayAdapter;
 
 import android.os.Bundle;
-//import android.widget.RadioButton;
-//import android.widget.RadioGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -19,52 +17,55 @@ import android.widget.TextView;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner noun;
-    ListView adverbsList;
+
+    String[] adj = { "Кривой", "Длинный", "Красивый", "Старый"};
+    String selectedItem;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Resources res = getResources();
-        String[] adjective = res.getStringArray(R.array.Adjective);
-
+        Spinner noun;
         noun = findViewById(R.id.spinner);
-        adverbsList = findViewById(R.id.list_view);
+        TextView text = findViewById(R.id.textView);
+        ListView adjectiveList;
+        adjectiveList = findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, adj);
+        adjectiveList.setAdapter(adapter);
 
-        ArrayAdapter<?> listView = ArrayAdapter.createFromResource(this,
-                R.array.Adjective,
-                android.R.layout.simple_spinner_item);
-        adverbsList.setAdapter(listView);
+        adjectiveList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                selectedItem = adj[position];
+//                text.setText(selectedItem);
 
+            }
+        });
 
         Resources noun_res = getResources();
         String[] noun_array = noun_res.getStringArray(R.array.Noun);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(
                 this,
                 R.array.Noun,
                 android.R.layout.simple_spinner_item
         );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        noun.setAdapter(adapter);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        noun.setAdapter(adapter1);
 
     }
 
     public void CreateText(View view) {
-        TextView textView = findViewById(R.id.textView);
+        TextView text = findViewById(R.id.textView);
         RadioGroup radioGroup = findViewById(R.id.radio_group);
-        noun = findViewById(R.id.spinner);
-        adverbsList = findViewById(R.id.list_view);
+        Spinner noun = findViewById(R.id.spinner);
         String test = noun.getSelectedItem().toString();
-        ListAdapter test2 = adverbsList.getAdapter();
-        int convert = Math.toIntExact(test6);
-        test2.getItemId();
-//        String selectedFromList = (String) (adverbsList.getItemAtPosition(test2));
         int test3 = radioGroup.getCheckedRadioButtonId();
         RadioButton radioButton = findViewById(test3);
         String test5 = radioButton.getText().toString();
-        String all =test2 + " "+ test +" "+ test5;
-        textView.setText(all);
+        String test1 = selectedItem;
+        String all =test1 + " " + test + " " + test5;
+        text.setText(all);
     }
+
 }
